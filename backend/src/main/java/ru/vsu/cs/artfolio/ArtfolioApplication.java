8ced94d3fd918e4e21db9a84215d842ed2e17f4a
Rafig <rafigdzabbarov0410@gmail.com>
2024-02-27 17:53:32 +0300

    backend: init

---
+++ backend/src/main/java/ru/vsu/cs/artfolio/ArtfolioApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.artfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtfolioApplication.class, args);
+	}
+
+}
