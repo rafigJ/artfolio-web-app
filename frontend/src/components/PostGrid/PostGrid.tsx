08ba6a24e9db2f03fa58c96bfcb30b0acbb7c7a8
ivan01239 <ivanbalyk01239@gmail.com>
2024-04-05 17:08:49 +0300

    frontend feat: infinity scroll

--- frontend/src/components/PostGrid/PostGrid.tsx
+++ frontend/src/components/PostGrid/PostGrid.tsx
@@ -1,21 +1,23 @@
-import { Button, List, Result } from 'antd'
+import { Button, List, Result, Skeleton } from 'antd'
 import React, { useEffect, useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
 import PostService from '../../api/PostService'
 import { useFetching } from '../../hooks/useFetching'
 import type { Product } from '../../types/MockTypes/Product'
 import PostCard from '../PostCard/PostCard'
 
 const PostGrid = () => {
+	const [page, setPage] = useState(0)
 	const [data, setData] = useState<Product[]>([])
 	
-	const [fetchPosts, isLoading, isError, error] = useFetching(async () => {
-		const response = await PostService.getProducts()
-		setData(response.data)
+	const [fetchPosts, isLoading, isError, error] = useFetching(async (_page) => {
+		const response = await PostService.getProducts(_page, 20)
+		setData([...data, ...response.data])
 	})
 	
 	useEffect(() => {
-		fetchPosts()
-	}, [])
+		fetchPosts(page)
+	}, [page])
 	
 	if (isError) {
 		return (
@@ -32,24 +34,32 @@ const PostGrid = () => {
 	}
 	
 	return (
-		<List
-			loading={isLoading}
-			grid={{
-				gutter: 16,
-				xs: 1,
-				sm: 2,
-				md: 4,
-				lg: 4,
-				xl: 4,
-				xxl: 4
-			}}
-			dataSource={data}
-			renderItem={(item) => (
-				<List.Item>
-					<PostCard key={item.id} product={item} />
-				</List.Item>
-			)}
-		/>
+		<InfiniteScroll
+			style={{ overflowX: 'hidden' }}
+			dataLength={data.length}
+			next={() => setPage(page + 1)}
+			hasMore={data.length < 100}
+			loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
+		>
+			<List
+				loading={isLoading}
+				grid={{
+					gutter: 16,
+					xs: 1,
+					sm: 2,
+					md: 4,
+					lg: 4,
+					xl: 4,
+					xxl: 4
+				}}
+				dataSource={data}
+				renderItem={(item) => (
+					<List.Item>
+						<PostCard key={item.id} product={item} />
+					</List.Item>
+				)}
+			/>
+		</InfiniteScroll>
 	)
 }
 
