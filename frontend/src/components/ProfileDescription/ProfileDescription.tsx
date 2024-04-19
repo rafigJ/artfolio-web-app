c3dbb83c01e46829266b5d6bd727278cf02cfe52
ivan01239 <ivanbalyk01239@gmail.com>
2024-04-19 12:16:41 +0300

    frontend add: export profile description button and exporting to pdf

--- frontend/src/components/ProfileDescription/ProfileDescription.tsx
+++ frontend/src/components/ProfileDescription/ProfileDescription.tsx
@@ -1,15 +1,44 @@
-import { HeartOutlined } from '@ant-design/icons'
-import { Descriptions, Flex, Statistic, Typography } from 'antd'
+import { FileTextOutlined, HeartOutlined } from '@ant-design/icons'
+import { Button, Descriptions, Flex, Statistic, Typography } from 'antd'
+import pdfMake from 'pdfmake/build/pdfmake'
+import pdfFonts from 'pdfmake/build/vfs_fonts'
 import { type FC } from 'react'
 import type { MockFullUserResponse } from '../../types/MockTypes/MockFullUserResponse'
 
 const { Paragraph } = Typography
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs
 
 interface ProfileDescriptionProps {
 	profile: MockFullUserResponse
 }
 
 const ProfileDescription: FC<ProfileDescriptionProps> = ({ profile }) => {
+	const handleExportDescription = () => {
+		const docDefinition = {
+			content: [
+				{ text: 'Имя пользователя: ', bold: true },
+				'Иван Васильевич',
+				{ text: 'Электронная почта: ', bold: true },
+				'ivanbalyk@mail.ru',
+				{ text: 'Город проживания: ', bold: true },
+				'Воронеж',
+				{ text: 'Страна проживания: ', bold: true },
+				'Россия',
+				{ text: 'Описание:', bold: true },
+				profile.description,
+				{ text: 'Количество лайков: ', bold: true },
+				profile.likeCount,
+				{ text: 'Количество публикаций: ', bold: true },
+				profile.postCount,
+				{ text: 'Количество подписчиков: ', bold: true },
+				profile.subscribersCount,
+			]
+		}
+
+		pdfMake.createPdf(docDefinition).download('profile_description.pdf')
+	}
+
 	return (
 		<Flex justify='space-evenly' align='center'>
 			<Flex vertical style={{ maxWidth: '60%' }}>
@@ -41,6 +70,12 @@ const ProfileDescription: FC<ProfileDescriptionProps> = ({ profile }) => {
 				<Paragraph>{profile.description}</Paragraph>
 			</Flex>
 			<Flex vertical>
+				<Button
+					icon={<FileTextOutlined />}
+					style={{ marginBottom: 20 }}
+					onClick={handleExportDescription}>
+					Экспортировать описание
+				</Button>
 				<Statistic
 					title='Количество лайков'
 					value={profile.likeCount}
@@ -52,7 +87,7 @@ const ProfileDescription: FC<ProfileDescriptionProps> = ({ profile }) => {
 					value={profile.subscribersCount}
 				/>
 			</Flex>
-		</Flex>
+		</Flex >
 	)
 }
 
