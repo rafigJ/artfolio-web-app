7fbf5bd94d0b756fa3ee69fc9dd5ef6c6b960719
Rafig <rafigdzabbarov0410@gmail.com>
2024-06-06 15:47:19 +0300

    backend add: AuthControllerTest

--- backend/src/test/java/ru/vsu/cs/artfolio/controller/AuthenticationControllerTest.java
+++ backend/src/test/java/ru/vsu/cs/artfolio/controller/AuthenticationControllerTest.java
@@ -2,8 +2,8 @@ package ru.vsu.cs.artfolio.controller;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,14 +12,22 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.mock.web.MockPart;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
 import ru.vsu.cs.artfolio.auth.user.Role;
 import ru.vsu.cs.artfolio.dto.auth.AuthRequestDto;
+import ru.vsu.cs.artfolio.dto.auth.RegisterRequestDto;
+import ru.vsu.cs.artfolio.entity.MediaFileEntity;
+import ru.vsu.cs.artfolio.repository.MediaRepository;
 import ru.vsu.cs.artfolio.service.PostServiceIT;
+import ru.vsu.cs.artfolio.service.impl.MinioService;
 
 import java.io.InputStream;
+import java.net.URI;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -29,6 +37,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class AuthenticationControllerTest {
 
     private static final InputStream mockFile = PostServiceIT.class.getClassLoader().getResourceAsStream("dummy-image.jpg");
+    private static final String AUTH_PATH = "/api/v1/auth";
     private static MockMultipartFile mockMultipartFile;
 
     @BeforeAll
@@ -37,13 +46,24 @@ public class AuthenticationControllerTest {
     }
 
     static final Logger LOG = LoggerFactory.getLogger(AuthenticationControllerTest.class);
+
+    MediaRepository mediaRepository;
+    MinioService minioService;
     MockMvc mockMvc;
     ObjectMapper objectMapper;
 
     @Autowired
-    public AuthenticationControllerTest(MockMvc mockMvc, ObjectMapper objectMapper) {
+    public AuthenticationControllerTest(MediaRepository mediaRepository, MinioService minioService, MockMvc mockMvc, ObjectMapper objectMapper) {
+        this.mediaRepository = mediaRepository;
+        this.minioService = minioService;
         this.mockMvc = mockMvc;
         this.objectMapper = objectMapper;
+    }
+
+    @AfterEach
+    void clearMinio() {
+        var mediaFiles = mediaRepository.findAll().stream().map(MediaFileEntity::getFileName).toList();
+        minioService.deleteFiles(mediaFiles);
     }
 
     @Test
@@ -54,7 +74,7 @@ public class AuthenticationControllerTest {
         LOG.info("json request {}", jsonRequestDto);
 
         // when
-        mockMvc.perform(post("/api/v1/auth/login")
+        mockMvc.perform(post(AUTH_PATH + "/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(jsonRequestDto))
                 // then
@@ -70,13 +90,37 @@ public class AuthenticationControllerTest {
 
 
     @Test
-    @Disabled
     void register_ValidDto_ReturnAuthResponse() throws Exception {
         // given
+        RegisterRequestDto requestDto = new RegisterRequestDto(
+                "full name",
+                "description",
+                "country",
+                "city",
+                "username",
+                "email@gmail.com",
+                "passwordSome12",
+                "secretWord"
+        );
+        String jsonRequestDto = objectMapper.writeValueAsString(requestDto);
+        LOG.info("json request {}", jsonRequestDto);
 
         // when
-
-        // then
+        mockMvc.perform(
+                multipart(AUTH_PATH + "/register")
+                        .file("file", mockMultipartFile.getBytes())
+                        .content(jsonRequestDto)
+                )
+                // then
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("$.username").value("username"),
+                        jsonPath("$.name").value("full name"),
+                        jsonPath("$.email").value("email@gmail.com"),
+                        jsonPath("$.role").value(Role.USER.name()),
+                        jsonPath("$.token").hasJsonPath()
+                );
     }
 
 
