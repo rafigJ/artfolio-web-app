be266726f05c25f62649fc8f4490edaab619d468
Rafig <rafigdzabbarov0410@gmail.com>
2024-06-16 17:49:55 +0300

    backend fix: set default time zone

--- backend/src/main/java/ru/vsu/cs/artfolio/ArtfolioApplication.java
+++ backend/src/main/java/ru/vsu/cs/artfolio/ArtfolioApplication.java
@@ -3,11 +3,14 @@ package ru.vsu.cs.artfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ArtfolioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArtfolioApplication.class, args);
-	}
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Moscow"));
+        SpringApplication.run(ArtfolioApplication.class, args);
+    }
 
 }
